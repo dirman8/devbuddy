6e0ada1a44de11a05edbc3df504c89bf531d115f
Sudirman <46485543+dirman8@users.noreply.github.com>
2023-06-01 11:16:50 +0700

    change createUserView to CreateUserView in import

--- src/components/CreateUserLogic.tsx
+++ src/components/CreateUserLogic.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form"
-import CreateUserView from "../createUserView"
+import CreateUserView from "./CreateUserView"
 import { yupResolver } from "@hookform/resolvers/yup";
 // import * as yup from "yup";
 
