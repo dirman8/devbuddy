55fb484e1b7a100e824e267ffe99567ce871723e
Sudirman <46485543+dirman8@users.noreply.github.com>
2023-08-16 19:18:59 +0700

    16 august 2023 update

--- src/pages/LoggedIn.tsx
+++ src/pages/LoggedIn.tsx
@@ -45,8 +45,6 @@ const LoggedIn = () => {
           </div>
           ))()
         }
-      {/* </div> */}
-
     </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <title>Dev Buddy</title>
         <meta name="description" content="A Web Developer Buddy " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/db.ico" />
       </Head>
 
       <main className={styles.main}>
